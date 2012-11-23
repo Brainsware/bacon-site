c50dcaac9682889db99f3c368bd85e84564f5f6b
jim <jim@0ffa3222-5024-11e0-b8f8-c3767ff1e5f8>
2012-11-23 00:43:03 +0000

    Updated from bacon trunk

--- htdocs/index.php
+++ htdocs/index.php
@@ -19,64 +19,111 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 'stdout');
 
-require_once __DIR__ . '/../config/bacon.php';
-
 # Start execution timer
 $baconStart = microtime();
 $baconStart = explode(' ', $baconStart);
 $baconStart = $baconStart[0] + $baconStart[1];
 
-# Root/App directory.
-\Config\Bacon::getBaconRoot();
-\Config\Bacon::getAppRoot();
+# This will take care of all including.
+# So basically, this is the only include/require you will ever see in this code (well, almost).
+require __DIR__ . '/../base/include.php';
 
-/**
- * This will take care of all including.
- * So basically, this is the only include/require you will ever see in this code (well, almost).
- */
-require BACON_ROOT . 'include.php';
+require \Sauce\Path::join(APP_ROOT, 'config/base.php');
 
-require BACON_ROOT . 'bacon/functions.php';
+$config = new \Sauce\ImmutableObject($base);
 
 # Set default timezone
-date_default_timezone_set(!empty(\Config\Base::$time_zone)? \Config\Base::$time_zone : 'UTC');
+date_default_timezone_set(!empty($config->timezone) ? $config->timezone : 'UTC');
 
 # Initialize log handler
-$log = new Bacon\Log();
+$log = new \Bacon\Log();
 
 # The default exception handler needs the log instance, too. See hack in base/include.php.
 default_exception_handler($log);
 
 # Enable logging in static-only and singleton classes (ugh..)
 Bacon\DB::setLog($log);
-Bacon\Path::setLog($log);
-
-# Initialize cache handler - everything must be cached!
-Bacon\Cache::init();
 
 # Session Handler
 # Initialize the session! Let's rock!
-$session = new \Bacon\Session(Config\Base::$session, $log);
+if (is_cli()) {
+	$session = new \Sauce\Object();
+} else {
+	$session = new \Bacon\Session($config->session, $log);
+}
 
-# Handle Request - translate from path to normal request, check language, call module.
-#Bacon\Request::handleRequest();
+$params = Ar($_REQUEST);
+$params->mergeF($_ENV);
+$params->mergeF($_SERVER);
 
-$request = new Bacon\Request(
-	$session,
-	array(
-		'base_uri'        => Config\Base::$base_uri,
-		'base_controller' => Config\Base::$base_controller,
-		'paths'           => array(
-			'styles'  => Config\Base::$styles_path,
-			'images'  => Config\Base::$images_path,
-			'scripts' => Config\Base::$scripts_path
-		)
-	),
-	$log
-);
+# PHP's builtin webserver doesn't know FallbackResource. So we have to do it on our
+# own in this case.
+#
+# Check whether given URI maps to a file or is to be handled by us.
+if (is_cli_server()) {
+	$path = $params->request_uri;
 
-$request->parse();
-$request->call();
+	# Remove any GET parameters (<uri>?foo=bar) if present
+	$question_mark = strpos($path, '?');
+
+	if ($question_mark !== false) {
+		$path = substr($path, 0, $question_mark);
+	}
+	
+	if (is_file(\Sauce\Path::join(HTDOCS, $path))) {
+		return false;
+	}
+
+	# We need to reset this since otherwise the SCRIPT_NAME would be set to
+	# the current requested URI. Which breaks like.. everything.
+	$_SERVER['SCRIPT_NAME'] = __FILE__;
+}
+
+
+# PHP forms the $_FILES array in the following way:
+# $_FILES => {
+# 	name => [ file1, file2, file3, ... ],
+# 	size => [ .... ],
+# 	...
+#
+# So we just transform it to:
+#
+# $files (\Sauce\Vector) => [
+# 	{
+# 		name => file1,
+# 		size => 1234,
+# 		...
+# 	},
+# 	{ ... }
+# ]
+#
+if (!empty($_FILES)) {
+	$files = new \Sauce\Vector();
+
+	foreach ($_FILES as $field => $uploads) {
+		$keys = array_keys($uploads);
+		$count = count($uploads[$keys[0]]);
+
+		$obj = new \Sauce\Object();
+
+		for ($i = 0; $i < $count; $i++) {
+			foreach ($keys as $key) {
+				$obj->$key = $uploads[$key][$i];
+			}
+
+			$obj->field = $field;
+		}
+
+		$files->push($obj);
+	}
+
+	$params->files = $files;
+}
+
+$app = new \Bacon\App($config, $session, $log, $params);
+
+$app->prepare(path_info(), http_method());
+$app->run();
 
 $baconEnd = microtime();
 $baconEnd = explode(' ', $baconEnd);
