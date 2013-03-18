c46ca121a9314ec1d66f53a7e52e3e9ff9d58cc0
Alexander Pánek <a.panek@brainsware.org>
2013-03-18 19:22:35 +0100

    Removed any values and sanitized comments

--- Config/Database.php
+++ Config/Database.php
@@ -5,19 +5,20 @@ namespace Config;
 class Database
 {
 	# This is the main database configuration
-	public static $main = array (
+	public static $main = [
 		# optional, defaults to localhost; NOTE: localhost != 127.0.0.1 [Difference: Access-method TCP/Sockets]
-		'server' => 'localhost',
-		'name' => 'baconphp.net',
+		'server'    => '',
+		'name'      => '',
+		'username'  => '',
+		'password'  => '',
+
 		# Anything YOUR PDO installation supports.
-		'type' => 'mysql',
-		# Don't use root. Please. (no, a user named urmom with all rights is not better.)
-		'username' => 'root',
-		'password' => '',
-		'persist' => false,
-		# only for mysql as of now
-		'encoding' => 'utf8',
-		);
+		'type'      => 'mysql',
+		'persist'   => false,
+		
+		# Currently only available for MySQL
+		'encoding'  => 'utf8',
+	];
 }
 
 ?>
